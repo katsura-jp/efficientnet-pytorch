e223691cbb89fb01777902b87acb3a6560d7981b
katsura_jp <nok.addayo@gmail.com>
2019-06-01 08:15:39 +0900

    add trainable param

--- model/swish.py
+++ model/swish.py
@@ -1,21 +1,26 @@
 import torch
 import torch.nn as nn
+from torch.nn.parameter import Parameter
 from torch._jit_internal import weak_module, weak_script_method
 
 
 @weak_module
 class Swish(nn.Module):
-    def __init__(self):
+    def __init__(self, train_beta=False):
         super(Swish, self).__init__()
+        if train_beta:
+            self.weight = Parameter(torch.Tensor([1.]))
+        else:
+            self.weight = 1.0
 
     @weak_script_method
     def forward(self, input):
-        return input * torch.sigmoid(input)
+        return input * torch.sigmoid(self.weight * input)
 
 
 def test():
     x = torch.FloatTensor(16, 128, 16, 16)
-    swish = Swish()
+    swish = Swish(train_beta=True)
     print(swish(x).size())
 
 if __name__ == '__main__':
