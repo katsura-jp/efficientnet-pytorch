0ef663827cbb826ed3a404e7950a6dc2003658e2
katsura_jp <nok.addayo@gmail.com>
2019-06-01 17:26:13 +0900

    add Stochastic Depth

--- model/efficientnet.py
+++ model/efficientnet.py
@@ -4,8 +4,6 @@ import torch.nn.functional as F
 
 from .swish import Swish
 
-#TODO: stochastic depth
-
 
 class SqeezeExcitation(nn.Module):
     def __init__(self, inplanes, se_ratio):
@@ -28,7 +26,7 @@ class SqeezeExcitation(nn.Module):
 
 
 class Bottleneck(nn.Module):
-    def __init__(self,inplanes, planes, kernel_size, stride, expand, se_ratio):
+    def __init__(self,inplanes, planes, kernel_size, stride, expand, se_ratio, prob=1.0):
         super(Bottleneck, self).__init__()
         if expand == 1:
             self.conv2 = nn.Conv2d(inplanes*expand, inplanes*expand, kernel_size=kernel_size, stride=stride,
@@ -49,8 +47,14 @@ class Bottleneck(nn.Module):
 
         self.swish = Swish()
         self.correct_dim = (stride == 1) and (inplanes == planes)
+        self.prob = torch.Tensor([prob])
 
     def forward(self, x):
+        if self.training:
+            if not torch.bernoulli(self.prob):
+                # drop
+                return x
+
         if hasattr(self, 'conv1'):
             out = self.conv1(x)
             out = self.bn1(out)
@@ -75,13 +79,20 @@ class Bottleneck(nn.Module):
 
 
 class MBConv(nn.Module):
-    def __init__(self, inplanes, planes, repeat, kernel_size, stride, expand, se_ratio):
+    def __init__(self, inplanes, planes, repeat, kernel_size, stride, expand, se_ratio, sum_layer, count_layer=None, pl=0.5):
         super(MBConv, self).__init__()
         layer = []
 
+        # not drop(stchastic depth)
         layer.append(Bottleneck(inplanes, planes, kernel_size, stride, expand, se_ratio))
-        for _ in range(1, repeat):
-            layer.append(Bottleneck(planes, planes, kernel_size, 1, expand, se_ratio))
+
+        for l in range(1, repeat):
+            if count_layer is None:
+                layer.append(Bottleneck(planes, planes, kernel_size, 1, expand, se_ratio))
+            else:
+                # stochastic depth
+                prob = 1.0 - (count_layer + l) / sum_layer * (1 - pl)
+                layer.append(Bottleneck(planes, planes, kernel_size, 1, expand, se_ratio, prob=prob))
 
         self.layer = nn.Sequential(*layer)
 
@@ -107,12 +118,15 @@ class Flatten(nn.Module):
 
 
 class EfficientNet(nn.Module):
-    def __init__(self, num_classes=1000, width_coef=1., depth_coef=1., scale=1.,dropout_ratio=0.2, se_ratio=0.25):
+    def __init__(self, num_classes=1000, width_coef=1., depth_coef=1., scale=1.,
+                 dropout_ratio=0.2, se_ratio=0.25, stochastic_depth=False, pl=0.5):
+
         super(EfficientNet, self).__init__()
         channels = [32, 16, 24, 40, 80, 112, 192, 320, 1280]
         expands = [1, 6, 6, 6, 6, 6, 6]
         repeats = [1, 2, 2, 3, 3, 4, 1]
         strides = [1, 2, 2, 2, 1, 2, 1]
+        kernel_sizes = [3, 3, 5, 3, 5, 5, 3]
         depth = depth_coef
         width = width_coef
 
@@ -120,19 +134,54 @@ class EfficientNet(nn.Module):
         channels = [round(x*width) for x in channels] # [int(x*width) for x in channels]
         repeats = [round(x*depth) for x in repeats] # [int(x*width) for x in repeats]
 
+        sum_layer = sum(repeats)
+
         self.upsample = Upsample(scale)
         self.swish = Swish()
 
         self.stage1 = nn.Sequential(
             nn.Conv2d(3, channels[0], kernel_size=3, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(channels[0], momentum=0.99, eps=1e-3))
-        self.stage2 = MBConv(channels[0], channels[1], repeats[0], kernel_size=3, stride=strides[0], expand=expands[0], se_ratio=se_ratio)
-        self.stage3 = MBConv(channels[1], channels[2], repeats[1], kernel_size=3, stride=strides[1], expand=expands[1], se_ratio=se_ratio)
-        self.stage4 = MBConv(channels[2], channels[3], repeats[2], kernel_size=5, stride=strides[2], expand=expands[2], se_ratio=se_ratio)
-        self.stage5 = MBConv(channels[3], channels[4], repeats[3], kernel_size=3, stride=strides[3], expand=expands[3], se_ratio=se_ratio)
-        self.stage6 = MBConv(channels[4], channels[5], repeats[4], kernel_size=5, stride=strides[4], expand=expands[4], se_ratio=se_ratio)
-        self.stage7 = MBConv(channels[5], channels[6], repeats[5], kernel_size=5, stride=strides[5], expand=expands[5], se_ratio=se_ratio)
-        self.stage8 = MBConv(channels[6], channels[7], repeats[6], kernel_size=3, stride=strides[6], expand=expands[6], se_ratio=se_ratio)
+
+        if stochastic_depth:
+            # stochastic depth
+            self.stage2 = MBConv(channels[0], channels[1], repeats[0], kernel_size=kernel_sizes[0],
+                                 stride=strides[0], expand=expands[0], se_ratio=se_ratio, sum_layer=sum_layer,
+                                 count_layer=sum(repeats[:0]), pl=pl)
+            self.stage3 = MBConv(channels[1], channels[2], repeats[1], kernel_size=kernel_sizes[1],
+                                 stride=strides[1], expand=expands[1], se_ratio=se_ratio, sum_layer=sum_layer,
+                                 count_layer=sum(repeats[:1]), pl=pl)
+            self.stage4 = MBConv(channels[2], channels[3], repeats[2], kernel_size=kernel_sizes[2],
+                                 stride=strides[2], expand=expands[2], se_ratio=se_ratio, sum_layer=sum_layer,
+                                 count_layer=sum(repeats[:2]), pl=pl)
+            self.stage5 = MBConv(channels[3], channels[4], repeats[3], kernel_size=kernel_sizes[3],
+                                 stride=strides[3], expand=expands[3], se_ratio=se_ratio, sum_layer=sum_layer,
+                                 count_layer=sum(repeats[:3]), pl=pl)
+            self.stage6 = MBConv(channels[4], channels[5], repeats[4], kernel_size=kernel_sizes[4],
+                                 stride=strides[4], expand=expands[4], se_ratio=se_ratio, sum_layer=sum_layer,
+                                 count_layer=sum(repeats[:4]), pl=pl)
+            self.stage7 = MBConv(channels[5], channels[6], repeats[5], kernel_size=kernel_sizes[5],
+                                 stride=strides[5], expand=expands[5], se_ratio=se_ratio, sum_layer=sum_layer,
+                                 count_layer=sum(repeats[:5]), pl=pl)
+            self.stage8 = MBConv(channels[6], channels[7], repeats[6], kernel_size=kernel_sizes[6],
+                                 stride=strides[6], expand=expands[6], se_ratio=se_ratio, sum_layer=sum_layer,
+                                 count_layer=sum(repeats[:6]), pl=pl)
+        else:
+            self.stage2 = MBConv(channels[0], channels[1], repeats[0], kernel_size=kernel_sizes[0],
+                                 stride=strides[0], expand=expands[0], se_ratio=se_ratio, sum_layer=sum_layer)
+            self.stage3 = MBConv(channels[1], channels[2], repeats[1], kernel_size=kernel_sizes[1],
+                                 stride=strides[1], expand=expands[1], se_ratio=se_ratio, sum_layer=sum_layer)
+            self.stage4 = MBConv(channels[2], channels[3], repeats[2], kernel_size=kernel_sizes[2],
+                                 stride=strides[2], expand=expands[2], se_ratio=se_ratio, sum_layer=sum_layer)
+            self.stage5 = MBConv(channels[3], channels[4], repeats[3], kernel_size=kernel_sizes[3],
+                                 stride=strides[3], expand=expands[3], se_ratio=se_ratio, sum_layer=sum_layer)
+            self.stage6 = MBConv(channels[4], channels[5], repeats[4], kernel_size=kernel_sizes[4],
+                                 stride=strides[4], expand=expands[4], se_ratio=se_ratio, sum_layer=sum_layer)
+            self.stage7 = MBConv(channels[5], channels[6], repeats[5], kernel_size=kernel_sizes[5],
+                                 stride=strides[5], expand=expands[5], se_ratio=se_ratio, sum_layer=sum_layer)
+            self.stage8 = MBConv(channels[6], channels[7], repeats[6], kernel_size=kernel_sizes[6],
+                                 stride=strides[6], expand=expands[6], se_ratio=se_ratio, sum_layer=sum_layer)
+
         self.stage9 = nn.Sequential(
                             nn.Conv2d(channels[7], channels[8], kernel_size=1, bias=False),
                             nn.BatchNorm2d(channels[8], momentum=0.99, eps=1e-3),
@@ -193,7 +242,8 @@ def efficientnet_b7(num_classes=1000):
 
 def test():
     x = torch.FloatTensor(64, 3, 224, 224)
-    model = efficientnet_b3()
+    model = EfficientNet(num_classes=100, width_coef=1.0, depth_coef=1.0, scale=1.0,dropout_ratio=0.2,
+                         se_ratio=0.25, stochastic_depth=True)
     logit = model(x)
     print(logit.size())
 
